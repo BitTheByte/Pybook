c29c394927560a619a15d3fe385ca3afce479e53
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2018-11-19 00:17:35 +0200

    Update helper.py

--- lib/helper.py
+++ lib/helper.py
@@ -2,22 +2,15 @@ import random
 import datetime
 
 def FindInDict(tkey,dict_,rand=0):
-
-
 	matches = []
 	for key in dict_:
 		if tkey == key:
 			return key
-
 	for key in dict_:
 		if tkey.find(key) != -1:
 			matches.append(key)
-
-
 	if rand != 0:
 		return matches[random.randint(0,len(matches)-1)]
-	
-	
 	return matches
 
 
@@ -25,4 +18,3 @@ def FindInDict(tkey,dict_,rand=0):
 def logger(out):
 	outtext= "[{}] {}".format(datetime.datetime.now(),out)
 	print outtext
-
